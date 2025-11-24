2bdc0b7cb5fb1c680ddb1aa486dbf8deba7456e3
ekaterina <e.gmerina@g.nsu.ru>
2025-11-25 01:44:31 +0700

    v5 finally

--- catlib/models/seismic_waves/seismic_waves_sim.c
+++ catlib/models/seismic_waves/seismic_waves_sim.c
@@ -43,19 +43,17 @@ int collision(void *n)
     return 0;
 }
 
-// === ЛОГИКА ПЕРЕМЕЩЕНИЯ (PROPAGATION) ===
-// Используем двойную буферизацию, чтобы избежать гонок данных
+
 void propagation_step(int width, int height)
 {
-    // Временный массив для следующего шага
-    // row-major order: index = j * width + i
+   
     uint8_t *next_grid = (uint8_t *)calloc(width * height, sizeof(uint8_t));
     if (!next_grid)
         exit(1);
 
     cellBody cell;
 
-    // 1. Чтение и разлет частиц
+    
     for (int i = 0; i < width; i++)
     {
         for (int j = 0; j < height; j++)
@@ -65,15 +63,13 @@ void propagation_step(int width, int height)
             if (b == 0)
                 continue;
 
-            // Расчет индексов соседей с периодическими граничными условиями (Тор)
-            // Или жесткими стенками (здесь сделаем жесткие стенки - поглощение)
 
             int i_next = i + 1;
             int i_prev = i - 1;
             int j_next = j + 1;
             int j_prev = j - 1;
 
-            // POSITIVE PARTICLES
+           
             if ((b & P_RIGHT) && (i_next < width))
                 next_grid[j * width + i_next] |= P_RIGHT;
             if ((b & P_LEFT) && (i_prev >= 0))
@@ -83,7 +79,7 @@ void propagation_step(int width, int height)
             if ((b & P_UP) && (j_prev >= 0))
                 next_grid[j_prev * width + i] |= P_UP;
 
-            // NEGATIVE PARTICLES
+           
             if ((b & N_RIGHT) && (i_next < width))
                 next_grid[j * width + i_next] |= N_RIGHT;
             if ((b & N_LEFT) && (i_prev >= 0))
@@ -95,7 +91,7 @@ void propagation_step(int width, int height)
         }
     }
 
-    // 2. Запись обратно в CAT
+   
     for (int i = 0; i < width; i++)
     {
         for (int j = 0; j < height; j++)
@@ -106,72 +102,6 @@ void propagation_step(int width, int height)
     }
 
     free(next_grid);
-}
-
-int propagation(void *n)
-{
-    cellBody *cell = (cellBody *)n;
-
-    CAT_Index size = CAT_GetArraySize();
-    int W = size.i;
-    // int H = size.j; // Можно использовать для проверки границ
-
-    uint8_t new_bits = 0;
-
-    // --- ЛОГИКА "PULL" (ТЯНУТЬ) ---
-    // Мы находимся в ячейке 'cell'. Смотрим на соседей и забираем прилетающие частицы.
-
-    // 1. Частица P_RIGHT (летит ВПРАВО)
-    // Она должна прийти от соседа СЛЕВА (смещение -1)
-    // Проверка (cell - 1) предполагает, что мы не на левой границе массива!
-    // *Примечание: Без проверки границ i>0 это опасно, но делаем "как в примере"*
-    if ((cell + 2)->bits & P_RIGHT)
-    {
-        new_bits |= P_RIGHT;
-    }
-    // Аналогично для отрицательной частицы
-    if ((cell + 2)->bits & N_RIGHT)
-    {
-        new_bits |= N_RIGHT;
-    }
-
-    // 2. Частица P_LEFT (летит ВЛЕВО)
-    // Приходит от соседа СПРАВА (смещение +1)
-    if ((cell + 1)->bits & P_LEFT)
-    {
-        new_bits |= P_LEFT;
-    }
-    if ((cell + 1)->bits & N_LEFT)
-    {
-        new_bits |= N_LEFT;
-    }
-
-    // 3. Частица P_UP (летит ВВЕРХ, то есть уменьшает индекс j)
-    // Приходит от соседа СНИЗУ (смещение +W, так как j растет вниз)
-    if ((cell + 3)->bits & P_UP)
-    {
-        new_bits |= P_UP;
-    }
-    if ((cell + 3)->bits & N_UP)
-    {
-        new_bits |= N_UP;
-    }
-
-    // 4. Частица P_DOWN (летит ВНИЗ, увеличивает индекс j)
-    // Приходит от соседа СВЕРХУ (смещение -W)
-    if ((cell + 4)->bits & P_DOWN)
-    {
-        new_bits |= P_DOWN;
-    }
-    if ((cell + 4)->bits & N_DOWN)
-    {
-        new_bits |= N_DOWN;
-    }
-
-    // Записываем новое состояние
-    cell->bits = new_bits;
-
-    return 0;
 }
 
 int main(int argc, char *argv[])
@@ -208,12 +138,6 @@ int main(int argc, char *argv[])
             return 1;
         }
 
-        // flag = CAT_Iterate(propagation);
-        // if (flag)
-        // {
-        //     printf("\nError: %d, iteration: %d\n", flag, i);
-        //     return 1;
-        // }
         propagation_step(I, J);
     }
 
